79c0cc83f737c81451f7bdec91fda48bc953b6b0
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-02-21 06:06:02 +0545

    Configuring Jdbc Template to execute query

---
+++ src/main/java/com/spring/jdbc/Student.java
@@ -0,0 +1,65 @@
+package com.spring.jdbc;
+
+public class Student {
+
+	private int Id;
+	private String name;
+	private String city;
+	
+	//defult constructor
+	public Student() {
+		
+	}
+	
+	//parameterized constructor
+	public Student(int id, String name, String city) {
+		super();
+		Id = id;
+		this.name = name;
+		this.city = city;
+	}
+	
+	
+	//generate getter and setter
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+//generate to string method
+	@Override
+	public String toString() {
+		return "Student [Id=" + Id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
